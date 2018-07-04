6f8bc8cac78cd1bebaa6efd55d7fa03003eb9d02
root <Duarte Cancela>
2018-07-04 09:20:07 +0000

    First commit

---
+++ ds-backup.sh
@@ -0,0 +1,37 @@
+#!/bin/bash -       
+#title           :ds-backup.sh
+#description     :This script will make a backup of drupal 8 website 
+#                 directory, database file and send it to Dropbox with Rlone.
+#author		 :Duarte Cancela
+#date            :2018-07-03
+#version         :0.1    
+#usage		 :bash ds-backup.sh
+#notes           :Install Zip and Rclone to use this script.
+#bash_version    :4.4.19(1)-release
+#==============================================================================
+
+#Go to www directory.
+cd /var/www/
+
+#Compress Drupal website directory and create a backup directory.
+sudo tar -zcvf duartstudio_`date +%d-%m-%Y`.tar.gz duartstudio.com && sudo mkdir backup_`date +%d-%m-%Y`
+
+#Move compressed Drupal 8 directory to backup directory.
+sudo mv duartstudio_`date +%d-%m-%Y`.tar.gz ./backup_`date +%d-%m-%Y`
+
+# Enter inside Drupal 8 directory.
+cd duartstudio.com
+
+# Create a database backup with drush command and move it to backup directory.
+sudo drush sql:dump --result-file=../../duartstudio_drupal8.sql && cd .. 
+mv duartstudio_drupal8.sql ./backup_`date +%d-%m-%Y`
+
+# Compress the backup directory.
+sudo tar -zcvf backup_`date +%d-%m-%Y`.tar.gz backup_`date +%d-%m-%Y` &&
+
+#Backup directory file to Dropbox with Rclone.
+sudo rclone copy backup_`date +%d-%m-%Y`.tar.gz duartstudio:duartstudio-backup &&
+
+# Delete backup directory
+sudo rm -r backup_`date +%d-%m-%Y`
+
